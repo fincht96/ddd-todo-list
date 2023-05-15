fda1819a0c65aec351bf7807b9a9b01741dcf52b
fincht96 <fincht96@gmail.com>
2023-05-15 14:59:37 +0100

    feat(server): add sequelize suppport and test pg connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,29 @@
 import express from 'express';
+import { Sequelize } from 'sequelize';
 
-const app = express();
-const port = process.env.API_PORT;
+const init = async () => {
+  const app = express();
+  const port = process.env.API_PORT;
 
-app.get('/', (_req, res) => {
-  res.send('Hello world');
-});
+  app.get('/', (_req, res) => {
+    res.send('Hello world');
+  });
 
-app.listen(port, () => {
-  console.log('port', port);
-  console.log("I'm listening or am I");
-});
+  try {
+    const sequelize = new Sequelize(
+      'postgres://postgres:postgres@db:5432/postgres'
+    );
+
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    app.listen(port, () => {
+      console.log('port', port);
+      console.log("I'm listening or am I");
+    });
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+  }
+};
+
+init();
