cd2d55f618b05495e7d1c0f7daa2d38df7e8d746
fincht96 <fincht96@gmail.com>
2023-06-13 14:12:33 +0100

    feat(server): add 'create task' int test

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,7 +3,6 @@ import path from 'path';
 import { v1Router } from './api/v1';
 import bodyParser from 'body-parser';
 
-const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
@@ -15,6 +14,4 @@ app.use(express.static(path.join(process.cwd(), 'build')));
 // api
 app.use('/v1', v1Router);
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+export { app };
